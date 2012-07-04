199081302b90bb68337bf6108c625faeb70ecd54
Mathias Walker <mwa@sourcepole.ch>
2012-07-04 16:48:07 +0200

    ArcIntersection Control: support snapping; updated example with optional snap to grid

--- examples/arc-intersection.js
+++ examples/arc-intersection.js
@@ -1,4 +1,4 @@
-var map, control;
+var map, control, pointLayer;
 
 function init() {
     map = new OpenLayers.Map('map');
@@ -6,7 +6,7 @@ function init() {
     var wmsLayer = new OpenLayers.Layer.WMS( "OpenLayers WMS",
         "http://vmap0.tiles.osgeo.org/wms/vmap0?", {layers: 'basic'});
 
-    var pointLayer = new OpenLayers.Layer.Vector("Point Layer");
+    pointLayer = new OpenLayers.Layer.Vector("Point Layer");
     map.addLayers([wmsLayer, pointLayer]);
 
     control = new OpenLayers.Control.ArcIntersection(pointLayer);
@@ -32,4 +32,49 @@ function update() {
     control.setRadius(0, radius1);
     var radius2 = parseFloat(document.getElementById("radius2").value);
     control.setRadius(1, radius2);
+}
+
+// snap to grid example
+var snappingLayer = null;
+var snappingControl = null;
+
+function toggleSnapping(element) {
+    if (element.checked) {
+        if (snappingLayer == null) {
+            // create snapping layer
+            snappingLayer = new OpenLayers.Layer.PointGrid({
+                name: "Snap Grid",
+                dx: 3, dy: 3,
+                styleMap: new OpenLayers.StyleMap({
+                    pointRadius: 1,
+                    strokeColor: "#3333ff",
+                    strokeWidth: 1,
+                    fillOpacity: 1,
+                    fillColor: "#ffffff",
+                    graphicName: "square"
+                })
+            });
+        }
+        if (snappingControl == null) {
+            // create snapping control
+            snappingControl = new OpenLayers.Control.Snapping({
+                layer: pointLayer,
+                targets: [{
+                    layer: snappingLayer,
+                    tolerance: 15
+                }]
+            });
+        }
+        map.addLayer(snappingLayer);
+        snappingLayer.setMaxFeatures(200);
+        snappingControl.activate();
+    }
+    else {
+        if (snappingControl != null) {
+            snappingControl.deactivate();
+        }
+        if (snappingLayer != null) {
+            map.removeLayer(snappingLayer);
+        }
+    }
 }
