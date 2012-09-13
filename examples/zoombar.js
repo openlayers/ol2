5fa25b037a819f6cd2f61a329aada86fa7ef91ff
Stéphane Brunner <courriel@stephane-brunner.ch>
2012-09-13 11:05:11 +0200

    Add ZoomBar example with styling

---
+++ examples/zoombar.js
@@ -0,0 +1,31 @@
+var map = new OpenLayers.Map({
+    div: "map",
+    layers: [new OpenLayers.Layer.OSM("OSM", [
+        'http://a.tile.openstreetmap.org/${z}/${x}/${y}.png',
+        'http://b.tile.openstreetmap.org/${z}/${x}/${y}.png',
+        'http://c.tile.openstreetmap.org/${z}/${x}/${y}.png'
+    ], { transitionEffect: 'resize' })],
+    controls: [
+        new OpenLayers.Control.Navigation(),
+        new OpenLayers.Control.Attribution(),
+        new OpenLayers.Control.ZoomBar()
+    ],
+    center: [0, 0],
+    zoom: 1
+});
+
+var map2 = new OpenLayers.Map({
+    div: "map2",
+    layers: [new OpenLayers.Layer.OSM("OSM", [
+        'http://a.tile.openstreetmap.org/${z}/${x}/${y}.png',
+        'http://b.tile.openstreetmap.org/${z}/${x}/${y}.png',
+        'http://c.tile.openstreetmap.org/${z}/${x}/${y}.png'
+    ], { transitionEffect: 'resize' })],
+    controls: [
+        new OpenLayers.Control.Navigation(),
+        new OpenLayers.Control.Attribution(),
+        new OpenLayers.Control.ZoomBar()
+    ],
+    center: [0, 0],
+    zoom: 1
+});
