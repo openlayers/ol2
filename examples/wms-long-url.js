4469bbd7bc71a5df7d74b82320ea4d6c28e10e74
Bernhard Kiselka <bernhard.kiselka@prisma-solutions.at>
2016-11-25 00:55:00 +0100

    fix WMS with long URLs using POST and a gutter
    1) examples/wms-long-url.js: added additional overlay in example
    2) Tile/Image/IFrame.js:
    a) adapt style of iframe: use same idea as line 267-277 of Tile/Image.js (in getImage(), line 130ff.)
    b) make sure bounds are set for getImageSize() (in createRequestForm(), line 172)

--- examples/wms-long-url.js
+++ examples/wms-long-url.js
@@ -8,19 +8,26 @@ var base = new OpenLayers.Layer.WMS( "OpenLayers WMS",
     {tileOptions: {maxGetUrlLength: 2048}, transitionEffect: 'resize'}
 );
 var overlay = new OpenLayers.Layer.WMS("Overlay",
-    "http://suite.opengeo.org/geoserver/wms",
-    {layers: "usa:states", transparent: true, makeTheUrlLong: longText},
+    "http://demo.boundlessgeo.com/geoserver/wms",
+    {layers: "topp:states", transparent: true, makeTheUrlLong: longText},
     {ratio: 1, singleTile: true, tileOptions: {maxGetUrlLength: 2048}, transitionEffect: 'resize'}
 );
-map.addLayers([base, overlay]);
+var overlay2 = new OpenLayers.Layer.WMS("Overlay with gutter",
+    "http://demo.boundlessgeo.com/geoserver/wms",
+    {layers: "topp:states", transparent: true, makeTheUrlLong: longText},
+    {tileOptions: {maxGetUrlLength: 2048}, transitionEffect: 'resize', opacity: 0.5, gutter: 32}
+);
+map.addLayers([base, overlay, overlay2]);
 map.zoomToMaxExtent();
 
 // add behavior to dom elements
 document.getElementById("longurl").onclick = function() {
     base.mergeNewParams({makeTheUrlLong: longText});
     overlay.mergeNewParams({makeTheUrlLong: longText});
+    overlay2.mergeNewParams({makeTheUrlLong: longText});
 };
 document.getElementById("shorturl").onclick = function() {
     base.mergeNewParams({makeTheUrlLong: null});
     overlay.mergeNewParams({makeTheUrlLong: null});
+    overlay2.mergeNewParams({makeTheUrlLong: null});
 };
